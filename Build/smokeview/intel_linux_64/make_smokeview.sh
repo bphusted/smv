835b549465a346aaae841e43783d4f273bda7d54
=Bjarne Husted <=bjarne.husted@gmail.com>
2020-02-17 16:11:48 +0100

    Added 5 files

--- Build/smokeview/intel_linux_64/make_smokeview.sh
+++ Build/smokeview/intel_linux_64/make_smokeview.sh
@@ -7,5 +7,6 @@ set -e
 LIBDIR=../../LIBS/intel_linux_64/
 eval make -C ${LIBDIR} ${SMV_MAKE_OPTS} -f make_LIBS.make all
 
+export IFORT_COMPILER_LIB=/sw/easybuild/software/iccifort/2019.5.281/compilers_and_libraries_2019.5.281/linux/compiler/lib/intel64_lin
 make -f ../Makefile clean
 eval make -j 4 ${SMV_MAKE_OPTS} -f ../Makefile intel_linux_64
