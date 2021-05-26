d18986f1a2b0a4fcdabe12b591fa0fd910a2f457
=Bjarne Husted <=bjarne.husted@gmail.com>
2021-05-26 23:45:06 +0200

    updated smokediff makefile and scripts

--- Build/smokediff/intel_linux_64/make_smokediff.sh
+++ Build/smokediff/intel_linux_64/make_smokediff.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+module purge
+module load compiler/iccifort/2020.4.304
+export IFORT_COMPILER_LIB=/sw/easybuild/software/iccifort/2020.4.304/compilers_and_libraries/linux/lib/intel64_lin
 rm -f *.o smokediff_linux_64
 make -f ../Makefile intel_linux_64
